7512d9e3a08170bb8021ff258782a41508b6eeac
Ivan Contreras <ivanc.contre@gmail.com>
2021-09-06 14:29:24 -0300

    APP react en webserver

--- index.ts
+++ index.ts
@@ -7,17 +7,8 @@ const app: Application = express();
 // Directorio público
 app.use(express.static('public'));
 
-app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html');
-});
-
-app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html');
-});
-
 app.get('*', (req, res) => {
-    //res.sendFile(__dirname + '/public/404.html');
-    res.sendFile('public/404.html', {root: __dirname })
+    res.sendFile('public/index.html', {root: __dirname })
 });
 
 app.listen(process.env.PORT, () => {
